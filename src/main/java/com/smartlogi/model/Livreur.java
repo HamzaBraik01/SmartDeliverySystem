f7c459b0efadd814090526274afc5972c64aefbe
HamzaBraik01 <hamzabraik02@gmail.com>
2025-10-23 10:28:58 +0100

    SDMS-5: feat: create Livreur entity with id, nom, prenom, vehicule, telephone

--- src/main/java/com/smartlogi/model/Livreur.java
+++ src/main/java/com/smartlogi/model/Livreur.java
@@ -1,4 +1,62 @@
 package com.smartlogi.model;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "livreurs")
 public class Livreur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nom", nullable = false)
+    private String nom;
+
+    @Column(name = "prenom", nullable = false)
+    private String prenom;
+
+    @Column(name = "vehicule", nullable = false)
+    private String vehicule;
+
+    @Column(name = "telephone", unique = true)
+    private String telephone;
+
+    // Constructeurs
+    public Livreur() {}
+
+    public Livreur(String nom, String prenom, String vehicule, String telephone) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.vehicule = vehicule;
+        this.telephone = telephone;
+    }
+
+    // Getters et Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+
+    public String getPrenom() { return prenom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; }
+
+    public String getVehicule() { return vehicule; }
+    public void setVehicule(String vehicule) { this.vehicule = vehicule; }
+
+    public String getTelephone() { return telephone; }
+    public void setTelephone(String telephone) { this.telephone = telephone; }
+
+    @Override
+    public String toString() {
+        return "Livreur{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", vehicule='" + vehicule + '\'' +
+                ", telephone='" + telephone + '\'' +
+                '}';
+    }
 }
+
+
+
