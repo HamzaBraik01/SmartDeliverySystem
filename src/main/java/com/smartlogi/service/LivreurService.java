dd277d642bc8ee01b1a863047b0390d2b79a10c8
HamzaBraik01 <hamzabraik02@gmail.com>
2025-10-24 17:00:00 +0100

    SDMS-21: Add LivreurService interface

--- src/main/java/com/smartlogi/service/LivreurService.java
+++ src/main/java/com/smartlogi/service/LivreurService.java
@@ -1,4 +1,29 @@
 package com.smartlogi.service;
 
+import com.smartlogi.model.Livreur;
+import java.util.List;
+import java.util.Optional;
+
 public interface LivreurService {
+
+
+    Livreur creerLivreur(String nom, String prenom, String vehicule, String telephone);
+
+
+    Optional<Livreur> modifierLivreur(Long id, String nom, String prenom, String vehicule, String telephone);
+
+
+    Optional<Livreur> trouverParId(Long id);
+
+
+    List<Livreur> listerTousLesLivreurs();
+
+
+    boolean supprimerLivreur(Long id);
+
+
+    boolean telephoneExiste(String telephone);
+
+
+    Optional<Livreur> trouverParTelephone(String telephone);
 }
