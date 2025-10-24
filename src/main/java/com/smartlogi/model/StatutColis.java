a2bc39debf073be88a88c660c1561544a1318b5f
HamzaBraik01 <hamzabraik02@gmail.com>
2025-10-24 12:30:00 +0100

    SDMS-12: Add StatutColis enum for package status

--- src/main/java/com/smartlogi/model/StatutColis.java
+++ src/main/java/com/smartlogi/model/StatutColis.java
@@ -1,4 +1,17 @@
 package com.smartlogi.model;
 
 public enum StatutColis {
+    PREPARATION("En préparation"),
+    EN_TRANSIT("En transit"),
+    LIVRE("Livré");
+
+    private final String description;
+
+    StatutColis(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
