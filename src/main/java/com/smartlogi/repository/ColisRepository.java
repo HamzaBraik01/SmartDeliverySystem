f7fbb6c5b37327def6d81887910a723c528a7ca5
HamzaBraik01 <hamzabraik02@gmail.com>
2025-10-24 14:00:00 +0100

    SDMS-15: Add ColisRepository interface

--- src/main/java/com/smartlogi/repository/ColisRepository.java
+++ src/main/java/com/smartlogi/repository/ColisRepository.java
@@ -1,4 +1,33 @@
 package com.smartlogi.repository;
 
+import com.smartlogi.model.Colis;
+import com.smartlogi.model.StatutColis;
+import java.util.List;
+import java.util.Optional;
+
 public interface ColisRepository {
+
+
+    Colis save(Colis colis);
+
+
+    Optional<Colis> findById(Long id);
+
+
+    List<Colis> findAll();
+
+
+    void deleteById(Long id);
+
+
+    boolean existsById(Long id);
+
+
+    List<Colis> findByLivreurId(Long livreurId);
+
+
+    List<Colis> findByStatut(StatutColis statut);
+
+
+    List<Colis> findUnassignedColis();
 }
