47530103ce8625999e188d13158c809dc83f2239
HamzaBraik01 <hamzabraik02@gmail.com>
2025-10-24 15:00:00 +0100

    SDMS-17: Add LivreurRepository interface

--- src/main/java/com/smartlogi/repository/LivreurRepository.java
+++ src/main/java/com/smartlogi/repository/LivreurRepository.java
@@ -1,4 +1,26 @@
 package com.smartlogi.repository;
 
+import com.smartlogi.model.Livreur;
+import java.util.List;
+import java.util.Optional;
+
 public interface LivreurRepository {
+
+
+    Livreur save(Livreur livreur);
+
+
+    Optional<Livreur> findById(Long id);
+
+
+    List<Livreur> findAll();
+
+
+    void deleteById(Long id);
+
+
+    boolean existsById(Long id);
+
+
+    Optional<Livreur> findByTelephone(String telephone);
 }
